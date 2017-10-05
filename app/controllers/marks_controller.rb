5e9eac227013f1261fcb732c7365bd6f31e972a8
Igor Biryukov <balalaukin@gmail.com>
2017-10-05 16:28:59 +0300

    Add PostsController#create action

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -1,2 +1,16 @@
 class MarksController < ApplicationController
+  def create
+    mark = Mark.create(mark_params)
+    if mark.save
+      render json: { avg_rate: Mark.avg_post_rate(mark.post_id) }
+    else
+      render json: mark.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def mark_params
+    params.require(:mark).permit(:post_id, :value)
+  end
 end
--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,2 +1,6 @@
 class Mark < ApplicationRecord
+  def self.avg_post_rate(post_id)
+    select('avg(value) as av', :post_id).where(post_id: post_id).group(:post_id)
+                                        .map(&:av).first.to_f
+  end
 end
