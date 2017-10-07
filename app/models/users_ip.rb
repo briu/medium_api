5acf89e053ad49a247c78b6468ea84b164bcd189
Igor Biryukov <balalaukin@gmail.com>
2017-10-07 18:01:46 +0300

    Add seeds

--- app/models/users_ip.rb
+++ app/models/users_ip.rb
@@ -4,6 +4,7 @@ class UsersIp < ApplicationRecord
 
   def self.users_with_same_ip
     # SUBSELECT FOR ONE QUERY?
-    where(ip_id: select(:ip_id).group(:ip_id).having('count(*) > 1').pluck(:ip_id)).includes(:user, :ip)
+    ip_ids = select(:ip_id).group(:ip_id).having('count(*) > 1').pluck(:ip_id)
+    select(:user_id, :ip_id).where(ip_id: ip_ids).includes(:user, :ip)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+one_time_usage_ips_size = 100
+
+one_time_usage_ips = 1.upto(one_time_usage_ips_size).map { |cnt| "127.0.0.#{cnt}"}
+
+random_time_usage_ips = 1.upto(100).map { |cnt| "127.0.1.#{cnt}"}
+
+autors_logins = 1.upto(200).map { |cnt| Faker::Name.name }
+
+0.upto(200_000) do |cnt|
+  ip = (cnt > one_time_usage_ips_size) ? random_time_usage_ips.sample : one_time_usage_ips[cnt]
+  params = {
+    post_params: { title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph },
+    author_params: { login: autors_logins.sample },
+    ip: ip
+  }
+
+  PostBuilder.new(params).create
+end
